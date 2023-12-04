2b05b75d6cfcf9ca1a48f85dcf8be8a1fc2e1763
INGENIO <ingeniokhalif@gmail.com>
2023-12-04 15:31:10 +0100

    created person schema

--- myApp.js
+++ myApp.js
@@ -1,12 +1,13 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
+const personSchema = require("./models/person.model");
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-let Person;
+let Person = mongoose.model("Person", personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
